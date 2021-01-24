3ebee04494fc6ececa2e6aa72455efefeee05542
haruki-stevefestl <stevefestl@gmail.com>
2021-01-24 20:31:21 +0800

    Remove global scope; reduce alias search code

--- SF7N-Resources/SF7N-Functions.ps1
+++ SF7N-Resources/SF7N-Functions.ps1
@@ -21,10 +21,10 @@ function Search-CSV {
     Update-GUI
 
     # Input Assist
-    $global:SearchTerms = [PSCustomObject] @{}
+    $SearchTerms = [PSCustomObject] @{}
     if ($wpf.InputAssist.IsChecked) {
         $csvHeader.foreach({ 
-            $global:SearchTerms | Add-Member $_ (
+            $SearchTerms | Add-Member $_ (
                 $wpf.$("TextBox_$_").Text -ireplace
                     ($csvAlias[1].$_, $csvAlias[0].$_) -ireplace
                     ($csvAlias[3].$_, $csvAlias[2].$_) -ireplace
@@ -33,7 +33,7 @@ function Search-CSV {
         })
     } else {
         $csvHeader.foreach({
-            $global:SearchTerms | Add-Member $_ $wpf.$("TextBox_$_").Text
+            $SearchTerms | Add-Member $_ $wpf.$("TextBox_$_").Text
         })
     }
     
@@ -56,23 +56,13 @@ function Search-CSV {
         $_.Remarks   -match $SearchTerms.Remarks
     }).Foreach{
         if ($wpf.AliasMode.IsChecked) {
-            $script:csvSearch.Add([PSCustomObject] @{
-                ID        =  $_.ID
-                Viewpoint =  $_.Viewpoint
-                Location  =  $_.Location
-                Collar    =  $_.Collar
-                Tie       =  $_.Tie
-                Skirt     =  $_.Skirt
-                Uniform   =  $_.Uniform
-                Subject   =  $_.Subject
-                Author    =  $_.Author
-                Remarks   =  $_.Remarks
-                NSFW      = ($_.NSFW    -replace $csvAlias[0].NSFW,$csvAlias[1].NSFW -replace $csvAlias[2].NSFW,$csvAlias[3].NSFW -replace $csvAlias[4].NSFW,$csvAlias[5].NSFW)
-                Mood      = ($_.Mood    -replace $csvAlias[0].Mood,$csvAlias[1].Mood -replace $csvAlias[2].Mood,$csvAlias[3].Mood -replace $csvAlias[4].Mood,$csvAlias[5].Mood)
-                Time      = ($_.Time    -replace $csvAlias[0].Time,$csvAlias[1].Time -replace $csvAlias[2].Time,$csvAlias[3].Time -replace $csvAlias[4].Time,$csvAlias[5].Time)
-                Sleeve    = ($_.Sleeve  -replace $csvAlias[0].Sleeve,$csvAlias[1].Sleeve -replace $csvAlias[2].Sleeve,$csvAlias[3].Sleeve)
-                Gender    = ($_.Gender  -replace $csvAlias[0].Gender,$csvAlias[1].Gender -replace $csvAlias[2].Gender,$csvAlias[3].Gender)
-            })
+            $tempRow = $_.PsObject.Copy()
+            $tempRow.NSFW   = ($_.NSFW   -replace $csvAlias[0].NSFW,$csvAlias[1].NSFW -replace $csvAlias[2].NSFW,$csvAlias[3].NSFW -replace $csvAlias[4].NSFW,$csvAlias[5].NSFW)
+            $tempRow.Mood   = ($_.Mood   -replace $csvAlias[0].Mood,$csvAlias[1].Mood -replace $csvAlias[2].Mood,$csvAlias[3].Mood -replace $csvAlias[4].Mood,$csvAlias[5].Mood)
+            $tempRow.Time   = ($_.Time   -replace $csvAlias[0].Time,$csvAlias[1].Time -replace $csvAlias[2].Time,$csvAlias[3].Time -replace $csvAlias[4].Time,$csvAlias[5].Time)
+            $tempRow.Sleeve = ($_.Sleeve -replace $csvAlias[0].Sleeve,$csvAlias[1].Sleeve -replace $csvAlias[2].Sleeve,$csvAlias[3].Sleeve)
+            $tempRow.Gender = ($_.Gender -replace $csvAlias[0].Gender,$csvAlias[1].Gender -replace $csvAlias[2].Gender,$csvAlias[3].Gender)
+            $script:csvSearch.Add($tempRow)
         } else {
             $script:csvSearch.Add($_)
         }
