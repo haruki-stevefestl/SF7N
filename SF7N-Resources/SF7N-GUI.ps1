feed051d16c43ee55b24dbba7ae903ae9ae2e532
haruki-stevefestl <stevefestl@gmail.com>
2021-01-25 16:24:32 +0800

    Preliminary edit mode tests

--- SF7N-Resources/SF7N-GUI.ps1
+++ SF7N-Resources/SF7N-GUI.ps1
@@ -1,5 +1,21 @@
 #—————————————————————————————————————————————————————————————————————————————+—————————————————————
+# Search-related actions
 $wpf.Search.Add_Click({Search-CSV})
 
 $wpf.CSVGrid.Add_MouseUp({Set-Preview $wpf.CSVGrid.SelectedItem.ID})
 $wpf.CSVGrid.Add_Keyup({Set-Preview $wpf.CSVGrid.SelectedItem.ID})
+
+# Editing-related actions
+$editingMode = $false
+$wpf.CSVGrid.Add_BeginningEdit({
+    $script:editingMode = $true
+    $wpf.Toolbar.SelectedIndex = 1
+    Write-Log 'DBG' 'Editing Mode On'
+})
+
+# On Commit/Commit&Return
+# $wpf.CSVGrid.Add_RowEditEnding({
+    # $script:editingMode = $false
+    # $wpf.Toolbar.SelectedIndex = 0
+    # Write-Log 'DBG' 'Editing Mode Off'
+# })
