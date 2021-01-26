f67c2aa7886f7b7cf5db7b34fe97cd22b1c3ef73
haruki-stevefestl <stevefestl@gmail.com>
2021-01-26 20:42:54 +0800

    Foolproof Export-CustomCSV; prepare Invoke-ChangeRow

--- SF7N-Resources/SF7N-Functions-Edit.ps1
+++ SF7N-Resources/SF7N-Functions-Edit.ps1
@@ -1,3 +1,17 @@
+#—————————————————————————————————————————————————————————————————————————————+—————————————————————
 function Export-CustomCSV ($ExportTo) {
-    $wpf.CSVGrid.Items | Export-CSV $ExportTo -NoTypeInformation
+    try {
+        $wpf.CSVGrid.Items | Export-CSV $ExportTo -NoTypeInformation
+    } catch {Write-Log 'Export CSV Failed'}
+}
+
+function Invoke-ChangeRow {
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateSet('InsertAbove','InsertBelow','InsertLast','RemoveAt')]
+        [String] $Action,
+
+        [Parameter(Mandatory=$true)][Int] $At,
+        [Parameter(Mandatory=$true)][Int] $Count
+    )
 }
