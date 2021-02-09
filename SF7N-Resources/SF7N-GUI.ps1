36890c818dc76123668d06e73082f4fd1af8f371
haruki-stevefestl <77907336+haruki-stevefestl@users.noreply.github.com>
2021-02-09 14:45:56 +0800

    I forgot ResetSorting button exists

--- SF7N-Resources/SF7N-GUI.ps1
+++ SF7N-Resources/SF7N-GUI.ps1
@@ -5,6 +5,11 @@ $wpf.Search.Add_Click({Search-CSV})
 $wpf.CSVGrid.Add_MouseUp({Set-Preview})
 $wpf.CSVGrid.Add_Keyup({Set-Preview})
 
+$wpf.ResetSorting.Add_Click({
+    $wpf.CSVGrid.Items.SortDescriptions.Clear()
+    $wpf.CSVGrid.Columns.ForEach({$_.SortDirection = $null})
+})
+
 #——————————————————————————————————————————————————————————————————————————————+————————————————————
 # Editing-related actions
 # Enter edit mode
