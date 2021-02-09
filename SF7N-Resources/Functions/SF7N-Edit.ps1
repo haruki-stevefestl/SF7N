6f758768e8af61c6d70f9ce842482607908afe31
haruki-stevefestl <77907336+haruki-stevefestl@users.noreply.github.com>
2021-02-09 22:38:02 +0800

    Reimplement InsertLast; Rename Invoke-ChangeRow

--- SF7N-Resources/Functions/SF7N-Edit.ps1
+++ SF7N-Resources/Functions/SF7N-Edit.ps1
@@ -5,7 +5,7 @@ function Export-CustomCSV ($ExportTo) {
     } catch {Write-Log 'ERR' 'Export CSV Failed'}
 }
 
-function Invoke-ChangeRow {
+function Add-Row {
     param(
         [Parameter(Mandatory=$true)]
         [ValidateSet('InsertAbove','InsertBelow','InsertLast')]
@@ -13,45 +13,46 @@ function Invoke-ChangeRow {
     )
 
     # Prepare blank template for inserting
-    $SCRIPT:RowTemplate = [PSCustomObject] @{}
-    $script:csvHeader.foreach{$RowTemplate | Add-Member NoteProperty $_ ''}
+    $RowTemplate = [PSCustomObject] @{}
+    $csvHeader.foreach{$RowTemplate | Add-Member NoteProperty $_ ''}
 
-    $SCRIPT:At = $wpf.CSVGrid.Items.IndexOf($wpf.CSVGrid.SelectedCells[0].Item)
+    $At = $wpf.CSVGrid.Items.IndexOf($wpf.CSVGrid.SelectedCells[0].Item)
 
     # Log in the correct format
-    # if ($Action -eq 'InsertLast') {
-    #     $SCRIPT:Count = $wpf.InsertLastCount.Text
-        # Get index of last entry's leftmost item
-        # e.g. 12345678-9 -> 9
-        # [String] $SCRIPT:IDStart = $csv[-1].($csvHeader[0]).Split('-')[1]
+    if ($Action -eq 'InsertLast') {
+        $wpf.CSVGrid.ScrollIntoView($wpf.CSVGrid.Items[-1], $wpf.CSVGrid.Columns[0])
+        $Count = $wpf.InsertLastCount.Text
 
-        # if IDStart is integer, add one; else set to zero
-        # if ($SCRIPT:IDStart -match '^\d+?$') {
-        #     ++ [Int] $SCRIPT:IDStart
-        # } else {
-        #     [Int] $SCRIPT:IDStart = 0
-        # }
+        # Get starting index
+        # If there already are entries, add 1 to the starting index; else 0
+        $LastIndex = $csv[-1].($csvHeader[0])
+        if ($LastIndex.Split('-')[0] -eq (Get-Date -Format yyyyMMdd)) {
+            $LastIndex = [Int] ($LastIndex.Split('-')[1]) + 1
+        } else {
+            $LastIndex = 0
+        }
 
-        # Write-Log 'INF' "Change Rows: InsertLast for $Count rows"
-    # } else {
-        $SCRIPT:Count = $wpf.CSVGrid.SelectedCells.Count
-        if ($Action -eq 'InsertBelow') {$At = $At + $Count} # $At += $Count doesn't work
-        Write-Log 'INF' "Change Rows: $Action at $At for $Count rows"
-    # }
+    } else {
+        $Count = $wpf.CSVGrid.SelectedCells.Count
+        $LastIndex = 0
 
-    write-log 'DBG' "$Action"
-    for ($I = 0; $I -lt $Count; $I++) {
-        write-log 'DBG' "$Action" | out-host
-        # if ($Action -eq 'InsertLast') {
-            # Add $Count rows at end with IDing
-            # $ThisRow = $RowTemplate.PsObject.Copy()
-            # $ThisRow.($csvHeader[0]) = "$(Get-Date -Format yyyyMMdd)-$I"
-            # $script:csv.Add($ThisRow)
-            # $script:csv.Add($RowTemplate)
-        # } else {
+        # $At += $Count doesn't work
+        if ($Action -eq 'InsertBelow') {$At = $At + $Count} 
+    }
+
+    Write-Log 'INF' "Change Rows: $Action at $At for $Count rows"
+    for ($I = $LastIndex; $I -lt ($LastIndex + $Count); $I++) {
+        if ($Action -eq 'InsertLast') {
+            # Add rows at end with IDing
+            $ThisRow = $RowTemplate.PsObject.Copy()
+            $ThisRow.($csvHeader[0]) = "$(Get-Date -Format yyyyMMdd)-$I"
+            $script:csv.Add($ThisRow)
+
+        } else {
             # Max & Min functions to prevent under/overflowing
-            $script:csv.Insert([Math]::Max(0,[Math]::Min($At,$script:csv.Count)), $RowTemplate)
-        # }
+            $script:csv.Insert(
+                [Math]::Max(0,[Math]::Min($At,$script:csv.Count)), $RowTemplate)
+        }
     }
 
     $wpf.CSVGrid.ItemsSource = $script:csv
