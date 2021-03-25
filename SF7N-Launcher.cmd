d70861ede3e84d9dd3b9cf3ee4f513d4d4583646
haruki-taka8 <stevefestl@gmail.com>
2021-03-25 16:49:30 +0800

    Reahoy!

---
+++ SF7N-Launcher.cmd
@@ -0,0 +1,16 @@
+@echo off
+set SF7NLocation="%~dp0\SF7N-Resources\SF7N-Loader.ps1"
+
+if "%OS%" NEQ "Windows_NT" call :error "Unsupported OS" "Only Windows NT-based OSes are supported."
+if not exist "%SF7NLocation%" call :error "SF7N Missing" "Update the path in variable SF7NLocation in %~dpf0"
+
+powershell.exe -File "%SF7NLocation%"
+exit /b
+
+:error
+echo Error: %~1
+echo(
+echo %~2
+echo Please press any key to exit
+>nul pause
+exit
