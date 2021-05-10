201c73a9d1218aec77503aebd821d7550cc622f4
haruki-taka8 <stevefestl@gmail.com>
2021-05-10 18:34:46 +0800

    Unahoy!

--- SF7N-Resources/Handlers/H-Global.ps1
+++
@@ -1,9 +0,0 @@
-#-----------------------------------------------------------------------------+---------------------
-# Global actions
-$wpf.PreviewCopy.Add_Click({
-    if ($null -ne $wpf.PreviewImage.Source) {
-        [Windows.Forms.Clipboard]::SetImage([Drawing.Image]::FromFile(
-            $wpf.PreviewImage.Source -replace 'file:///',''
-        ))
-    }
-})
