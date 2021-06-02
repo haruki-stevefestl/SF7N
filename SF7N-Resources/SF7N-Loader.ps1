2d4af16b1a755bcd28f9f8d535c9042e6abcf404
haruki-taka8 <stevefestl@gmail.com>
2021-06-02 21:28:22 +0800

    Reorder code to maximize GUI time

--- SF7N-Resources/SF7N-Loader.ps1
+++ SF7N-Resources/SF7N-Loader.ps1
@@ -9,31 +9,39 @@ Write-Log 'INF' '-- SF7N Initialization --'
 Write-Log 'INF' 'Import WPF'
 Add-Type -AssemblyName PresentationFramework
 
-# Read and evaluate path configurations
-Write-Log 'INF' 'Import Configurations'
-$config = Get-Content .\Configurations\General.ini | ConvertFrom-StringData
-$csvLocation = $ExecutionContext.InvokeCommand.ExpandString($config.csvLocation)
-$previewLocation = $ExecutionContext.InvokeCommand.ExpandString($config.previewPath)
-
 # Load a WPF GUI from a XAML file
 Write-Log 'INF' 'Parse  XAML'
 [Xml] $xaml = Get-Content .\GUI.xaml
 $tempform = [Windows.Markup.XamlReader]::Load([Xml.XmlNodeReader]::New($xaml))
 $wpf = [Hashtable]::Synchronized(@{})
+$ErrorActionPreference = 'SilentlyContinue'
 $xaml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]").Name.
     ForEach{$wpf.Add($_, $tempform.FindName($_))}
+$ErrorActionPreference = 'Continue'
 
 # Import GUI Control code
 Write-Log 'INF' 'Import Modules'
-Get-ChildItem *.ps1 -Recurse -Exclude SF7N-Loader.ps1 | Import-Module
+Import-Module (Get-ChildItem *.ps1 -Recurse -Exclude SF7N-Loader.ps1)
 
 # Initialzation work after splashscreen show
 $wpf.SF7N.Add_ContentRendered{
     Write-Log 'INF' 'Import WinForms'
     Add-Type -AssemblyName System.Windows.Forms, System.Drawing 
 
+    # Read and evaluate path configurations
+    Write-Log 'INF' 'Import Configurations'
+    $config = Get-Content .\Configurations\General.ini | ConvertFrom-StringData
+    $script:csvLocation = $ExecutionContext.InvokeCommand.ExpandString($config.csvLocation)
+    $script:previewLocation = $ExecutionContext.InvokeCommand.ExpandString($config.previewPath)
+
     Import-CustomCSV $csvLocation
     $wpf.CSVGrid.ItemsSource = $csv
+    
+    # Apply config
+    $wpf.AliasMode.IsChecked   = $config.AliasMode   -ieq 'true'
+    $wpf.InputAssist.IsChecked = $config.InputAssist -ieq 'true'
+    $wpf.ReadWrite.IsChecked   = $config.ReadWrite   -ieq 'true'
+    $wpf.InsertLastCount.Text  = $config.InsertLast
 
     # Generate columns of Datagrid
     Write-Log 'INF' 'Build  Datagrid'
@@ -61,13 +69,10 @@ $wpf.SF7N.Add_ContentRendered{
         $wpf.CSVGrid.Columns.Add($Column)
     }
 
-    $wpf.AliasMode.IsChecked   = $config.AliasMode   -ieq 'true'
-    $wpf.InputAssist.IsChecked = $config.InputAssist -ieq 'true'
-    $wpf.ReadWrite.IsChecked   = $config.ReadWrite   -ieq 'true'
-    $wpf.InsertLastCount.Text  = $config.InsertLast
-
+    # Cleanup
     $wpf.SplashScreen.Visibility = 'Hidden'
     Write-Log 'DBG' "Total  $(((Get-Date)-$startTime).TotalMilliseconds) ms"
+    Remove-Variable 'tempform', 'xaml', 'startTime' -Scope Script
 }
 
 # Prompt and cleanup on close
