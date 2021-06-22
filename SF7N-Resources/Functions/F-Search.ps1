6d7d34da5ac4a69783dbcd18a03c07947cb0e07c
haruki-taka8 <stevefestl@gmail.com>
2021-06-22 23:03:27 +0800

    Revamp F-Search.ps1
    * Move functions into code directly
    * Redo search term parsing
    * Simplify runspace creation

--- SF7N-Resources/Functions/F-Search.ps1
+++ SF7N-Resources/Functions/F-Search.ps1
@@ -1,15 +1,3 @@
-function ConvertFrom-Alias ($Row) {
-    if ($csvAlias) {
-        $Row.PSObject.Properties.Foreach{
-            $Header = $_.Name
-            for ($i = 0; $i -lt $csvAlias.$Header.Count; $i += 2) {
-                $_.Value = $_.Value.Replace($csvAlias[$i+1].$Header, $csvAlias[$i].$Header)
-            }
-        }
-    }
-    return $Row
-}
-
 function Search-CSV ($SearchText, $FirstRun) {
     # Initialize
     $wpf.CSVGrid.ItemsSource = $null
@@ -18,69 +6,70 @@ function Search-CSV ($SearchText, $FirstRun) {
 
     # Parse SearchRules Text into [PSCustomObject] $SearchTerm
     $SearchTerm = [PSCustomObject] @{}
+    $Regex = '(["'']?)(?(1)(.+?|[\S"'']+?))\1[:=](["'']?)(?(1)(.+?|[\S"'']+?))\3(?:\s|$)'
 
-    # While there are search terms in $SearchText
-    #     Add to $SearchTerm
-    #     Remove from $SearchText
-    while (
-        $SearchText -match
-        '(["'']?)(?(1)(.+?|[\S"'']+?))\1[:=](["'']?)(?(1)(.+?|[\S"'']+?))\3(?:\s|$)'
-    ) {
-        $SearchTerm | Add-Member -MemberType NoteProperty -Name $Matches[2] -Value $Matches[4]
-        $SearchText = $SearchText.Replace($Matches[0], '')
-    }
+    ($SearchText | Select-String $Regex -AllMatches).Matches.ForEach({
+        $SearchTerm |
+        Add-Member -MemberType NoteProperty -Name $_.Groups[2].Value -Value $_.Groups[4].Value
+    })
 
     # Apply input alias
-    if ($context.InputAlias) {$SearchTerm = ConvertFrom-Alias $SearchTerm}
-    Write-Log 'DBG' $context.OutputAlias
-
-    # Search with new runspace
-    $Runspace = [RunspaceFactory]::CreateRunspace()
-    $Runspace.ApartmentState = 'STA'
-    $Runspace.ThreadOptions = 'ReuseThread'
-    $Runspace.Open()
-    # Pass variables
-    ('wpf','csv','SearchTerm','csvAlias','context','FirstRun').ForEach({
-        $RunSpace.SessionStateProxy.SetVariable($_, (Get-Variable $_).Value)
-    })
-    $Ps = [PowerShell]::Create().AddScript{
-        function ConvertTo-Alias ($Row) {
-            if ($csvAlias) {
-                $Row.PSObject.Properties.Foreach{
-                    $Header = $_.Name
-                    for ($i = 0; $i -lt $csvAlias.$Header.Count; $i += 2) {
-                        $_.Value = $_.Value.Replace($csvAlias[$i].$Header, $csvAlias[$i+1].$Header)
-                    }
-                }
+    if ($context.InputAlias) {
+        $SearchTerm.PSObject.Properties.ForEach({
+            $Header = $_.Name
+            for ($i = 0; $i -lt $csvAlias.$Header.Count; $i += 2) {
+                $_.Value = $_.Value.Replace($csvAlias[$i+1].$Header, $csvAlias[$i].$Header)
             }
-            return $Row
+        })
+    }
+
+    # Search with new Powershell instance
+    $Ps = [PowerShell]::Create().AddScript{
+        function Update-GUI ([Action] $Action) {
+            $wpf.SF7N.Dispatcher.Invoke([Action] $Action, 'Normal')
         }
 
         [Collections.ArrayList] $CsvSearch = @()
         foreach ($Entry in $csv) {
             # If notMatch, goto next iteration
             if (!$FirstRun -or ('' -eq $SearchTerm)) {
-                $SearchTerm.PSObject.Properties.ForEach{
+                $SearchTerm.PSObject.Properties.ForEach({
                     if ($Entry.($_.Name) -notmatch $_.Value) {continue}
-                }
+                })
             }
         
             # Apply alias if OutputAlias is on; else add raw content
             if ($context.OutputAlias) {
-                $CsvSearch.Add((ConvertTo-Alias $Entry.PsObject.Copy()))
+                $Row = $Entry.PSObject.Copy()
+                $Row.PSObject.Properties.ForEach({
+                    $Header = $_.Name
+                    for ($i = 0; $i -lt $csvAlias.$Header.Count; $i += 2) {
+                        $_.Value = $_.Value.Replace($csvAlias[$i].$Header, $csvAlias[$i+1].$Header)
+                    }
+                })
+                $CsvSearch.Add($Row)
+                
             } else {
                 $CsvSearch.Add($Entry)
             }
 
             if ($CsvSearch.Count -eq 25) {
-                $wpf.SF7N.Dispatcher.Invoke([Action] {$wpf.CSVGrid.ItemsSource = $CsvSearch.PSObject.Copy()}, 'Normal')
+                Update-GUI {$wpf.CSVGrid.ItemsSource = $CsvSearch.PSObject.Copy()}
             }
         }
-        $wpf.SF7N.Dispatcher.Invoke([Action] {$wpf.CSVGrid.ItemsSource = $CsvSearch}, 'Normal')
+        Update-GUI {$wpf.CSVGrid.ItemsSource = $wpf.CSVGrid.ItemsSource = $CsvSearch}
         $context.Status = 'Ready'
-        $wpf.SF7N.Dispatcher.Invoke([Action] {$wpf.SF7N.DataContext = $null}, 'Normal')
-        $wpf.SF7N.Dispatcher.Invoke([Action] {$wpf.SF7N.DataContext = $context}, 'Normal')
+        Update-GUI {$wpf.SF7N.DataContext = $null}
+        Update-GUI {$wpf.SF7N.DataContext = $context}
     }
-    $Ps.Runspace = $Runspace
+    
+    # Assign runspace to instance
+    $Ps.Runspace = [RunspaceFactory]::CreateRunspace()
+    $Ps.Runspace.ApartmentState = 'STA'
+    $Ps.Runspace.ThreadOptions = 'ReuseThread'
+    $Ps.Runspace.Open()
+    (Get-Variable wpf,csv,SearchTerm,csvAlias,context,FirstRun).ForEach({
+        $Ps.Runspace.SessionStateProxy.SetVariable($_.Name, $_.Value)
+    })
     $Ps.BeginInvoke()
 }
